3ea56735da888c2beb8c414d68f4c629f83bd9b7
rpuerto <raqpuecar@gmail.com>
2026-05-13 18:02:28 +0200

    Creado entidad Task. La entidad será la que indique que cual es la informacion que queremos guardar

---
+++ demo-backend-moderno/src/main/java/com/example/demobackendmoderno/entities/TaskEntity.java
@@ -0,0 +1,20 @@
+package com.example.demobackendmoderno.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+}
