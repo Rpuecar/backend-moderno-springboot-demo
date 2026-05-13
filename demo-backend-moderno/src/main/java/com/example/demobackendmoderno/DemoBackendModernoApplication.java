458444fe99755fc0f14f1bab313c9b0d00202bc8
rpuerto <raqpuecar@gmail.com>
2026-05-13 17:54:27 +0200

    Creado proyecto demo-backend-moderno con spring boot

---
+++ demo-backend-moderno/src/main/java/com/example/demobackendmoderno/DemoBackendModernoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobackendmoderno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendModernoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBackendModernoApplication.class, args);
+    }
+
+}
---
+++ demo-backend-moderno/src/test/java/com/example/demobackendmoderno/DemoBackendModernoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobackendmoderno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendModernoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
